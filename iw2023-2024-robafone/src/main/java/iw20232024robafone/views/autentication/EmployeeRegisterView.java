c731d023376bb89ea0907731cfd7c1566ba3976d
Luis Fernando Torres Moya <luis.torresmo@alum.uca.es>
2024-01-04 23:21:00 +0100

    Update EmployeeRegisterView.java

--- iw2023-2024-robafone/src/main/java/iw20232024robafone/views/autentication/EmployeeRegisterView.java
+++ iw2023-2024-robafone/src/main/java/iw20232024robafone/views/autentication/EmployeeRegisterView.java
@@ -1,5 +1,6 @@
 package iw20232024robafone.views.autentication;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
@@ -7,16 +8,24 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import iw20232024robafone.backend.entity.Client;
+import iw20232024robafone.backend.entity.Employee;
+import iw20232024robafone.backend.service.ClientService;
+import iw20232024robafone.backend.service.EmployeeService;
+import iw20232024robafone.security.SecurityService;
+
+import java.util.List;
 
 @AnonymousAllowed
 @Route("employee_register")
 @PageTitle("Register for Employees| Robafone")
 public class EmployeeRegisterView extends VerticalLayout {
 
-    public EmployeeRegisterView(){
+    public EmployeeRegisterView(EmployeeService employeeService){
 
         //Set the layout to be centered in the page.
         setSizeFull();
@@ -25,6 +34,22 @@ public class EmployeeRegisterView extends VerticalLayout {
 
         //Main title of the page.
         H1 title = new H1("Register in Robafone");
+
+        TextField username = new TextField();
+        username.setLabel("Username");
+        username.setClearButtonVisible(true);
+
+        TextField firstName = new TextField();
+        firstName.setLabel("First Name");
+        firstName.setClearButtonVisible(true);
+
+        TextField lastName = new TextField();
+        lastName.setLabel("LastName");
+        lastName.setClearButtonVisible(true);
+
+        TextField phone = new TextField();
+        phone.setLabel("Phone Number");
+        phone.setClearButtonVisible(true);
 
         //Create the email form.
         EmailField validEmailField = new EmailField();
@@ -47,21 +72,55 @@ public class EmployeeRegisterView extends VerticalLayout {
         //Finally the button to be registered. The event is missing, will redirect to a register (Backend).
         Button primaryButton = new Button("Register",
                 //When the button is clicked, we check if the fields are OK. If then, the user is registered.
-                event -> Register(validEmailField.getValue(), passwordField.getValue(), repeatPasswordField.getValue()));
+                event -> Register(validEmailField.getValue(),
+                        passwordField.getValue(),
+                        repeatPasswordField.getValue(),
+                        username.getValue(),
+                        firstName.getValue(),
+                        lastName.getValue(),
+                        phone.getValue(),
+                        employeeService));
         primaryButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        add(title, validEmailField, passwordField, repeatPasswordField, primaryButton);
+        add(title, validEmailField,username,firstName,lastName,phone, passwordField, repeatPasswordField, primaryButton);
     }
 
-    private void Register(String email, String p1, String p2){
-        if(email.isEmpty()){
+    private void Register(String email, String p1, String p2, String username, String firstName, String lastname, String phone, EmployeeService employeeService) {
+        if (email.isEmpty()) {
             Notification.show("Email is empty");
-        }else if(p1.isEmpty()){
+        } else if (p1.isEmpty()) {
             Notification.show("Password is empty");
         } else if (!p1.equals(p2)) {
             Notification.show("Passwords are not equal");
         } else {
-            Notification.show("The user is registered, change later");
+            List<Employee> listaEmpleados = employeeService.findAll();
+
+            //List<Client> listaClientestTest = clientRepository.findAll();
+
+            Boolean existeEmpleado = false;
+
+            for (int i = 0; i < listaEmpleados.size() && existeEmpleado == false; i++) {
+                if (listaEmpleados.get(i).getEmail() == email) {
+                    existeEmpleado = true;
+                }
+            }
+
+            if (existeEmpleado != true) {
+                Employee newEmployee = new Employee();
+                newEmployee.setUsername(username);
+                newEmployee.setFirstName(firstName);
+                newEmployee.setLastName(lastname);
+                newEmployee.setPhoneNumber(phone);
+                newEmployee.setEmail(email);
+                newEmployee.setPassword(SecurityService.passwordEncoder().encode(p1));
+                newEmployee.setRole("EMPLOYEE");
+                employeeService.save(newEmployee);
+                Notification.show("The user is registered");
+                UI.getCurrent().navigate("internal");
+            } else {
+                Notification.show("The user already exists");
+
+            }
         }
     }
 }
